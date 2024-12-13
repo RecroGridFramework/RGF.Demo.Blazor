593c77b6480c3ec7c3cd0844542624ba5007a04c
Tamás Mikola <mikola.tamas@recrovit.hu>
2024-12-13 19:47:58 +0100

    8.14.0

---
+++ src/Configure.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Recrovit.RecroGridFramework.Client.Blazor;
+using Syncfusion.Blazor;
+
+namespace RGF.Demo.Blazor;
+
+public static class Configure
+{
+    public static WebAssemblyHostBuilder ConfigureServices(this WebAssemblyHostBuilder builder)
+    {
+        builder.Services.AddOidcAuthentication(options =>
+        {
+            var provider = builder.Configuration.GetValue<string>("Oidc:Provider", "Duende");
+            builder.Configuration.Bind($"Oidc:{provider}:ProviderOptions", options.ProviderOptions);
+            //builder.Configuration.Bind($"Oidc:{provider}:UserOptions", options.UserOptions);
+        });
+
+        var loggerFactory = builder.Services.BuildServiceProvider().GetRequiredService<ILoggerFactory>();
+        var logger = loggerFactory.CreateLogger<Program>();
+        builder.Services.AddRgfBlazorServices(builder.Configuration, logger);
+
+#if DevExpressEnabled
+        builder.Services.AddDevExpressBlazor(configure => configure.BootstrapVersion = DevExpress.Blazor.BootstrapVersion.v5);
+#endif
+        builder.Services.AddScoped<Radzen.DialogService>();
+
+        builder.Services.AddSyncfusionBlazor();
+        Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense(builder.Configuration.GetValue<string>("Syncfusion:LicenseKey"));
+
+#if TelerikEnabled
+        builder.Services.AddTelerikBlazor();
+#endif
+        return builder;
+    }
+}
---
+++ src/Program.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Recrovit.RecroGridFramework.Client.Blazor;
+using RGF.Demo.Blazor;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.ConfigureServices();
+var host = builder.Build();
+
+await host.Services.InitializeRgfBlazorAsync();
+
+await host.RunAsync();
